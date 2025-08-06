21b943e1da245fd137b6b2e6c40d758f1eb7af53
Uzma Farzana <143869878+03uzma@users.noreply.github.com>
2025-08-07 01:13:50 +0530

    Update Peer.js

--- frontend/src/Peer.js
+++ frontend/src/Peer.js
@@ -1,19 +1,3 @@
-// import Peer from "simple-peer";
-
-// export function createPeer(initiator, socket, roomId) {
-//   const peer = new Peer({
-//     initiator,
-//     trickle: false,
-//   });
-
-//   peer.on("signal", (data) => {
-//     socket.emit("signal", { signalData: data, roomId });
-//   });
-
-//   return peer;
-// }
-
-
 import Peer from "simple-peer";
 
 export function createPeer(initiator, stream = null, socket, roomId) {
