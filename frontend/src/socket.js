9e3daeeede05329400b8d1e2a729e1d777760f5f
Uzma Farzana <143869878+03uzma@users.noreply.github.com>
2026-05-23 20:33:58 +0530

    Use environment variable for socket URL

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://peer-room-lite-backend.onrender.com"); // Update if your server is on a different port
+const socket = io(import.meta.env.VITE_SOCKET_URL); // Update if your server is on a different port
 
 export default socket;
